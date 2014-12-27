45f7a8c93112b47f8553cfa7a9cb89b1ebe9af1c
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2014-12-27 09:47:17 +0100

    Add indexing and subcommands `index` & `solve`

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 ```bash
-# uses `/usr/share/dict/words` by default
-$ ./anagram.py silent
+# use default words file `/usr/share/dict/words`:
+$ ./anagram.py solve silent
 enlist
 listen
 tinsel
+
+# pass custom words file:
+$ ./anagram.py solve -w words silent
+
+# index words file for reuse
+$ ./anagram.py index words | gzip > words.idx.json.gz
+$ gunzip -c words.idx.json.gz | ./anagram.py solve -i - silent
 ```
--- anagram.py
+++ anagram.py
@@ -3,43 +3,74 @@
 import sys
 import os
 from collections import defaultdict
+import json
+from itertools import ifilter
 import click
 
-default_dict_file = '/usr/share/dict/words'
+default_words_file = '/usr/share/dict/words'
 
 
-def solve_with_db(query, db):
-    for word in db[''.join(sorted(query.lower()))]:
-        if word.lower() != query.lower():
-            yield word
+@click.group()
+def cli():
+    """Anagram solver."""
 
 
-def mk_db(fn):
-    db = defaultdict(list)
-    with open(fn) as f:
-        for l in f:
-            word = l.rstrip()
-            db[''.join(sorted(word.lower()))].append(word)
-    return db
+def idx_key(word):
+    return ''.join(sorted(word.lower()))
 
 
-def solve_with_file(query, fn):
-    db = mk_db(fn)
-    return solve_with_db(query, db)
+def mk_idx(f):
+    idx = defaultdict(list)
+    for l in f:
+        word = l.rstrip()
+        key = idx_key(word)
+        idx[key].append(word)
+    return idx
 
 
-@click.command()
-@click.option('dictionary', '-d', '--dict',
-              help='dictionary file',
-              type=click.Path(),
-              default=default_dict_file)
+@cli.command()
+@click.argument('wordsfile', type=click.File())
+def index(wordsfile):
+    """Index a words file.
+    \b
+    e.g.
+    index /usr/share/dict/words > words.idx.json
+    """
+    idx = mk_idx(wordsfile)
+    click.echo(json.dumps(idx))
+
+
+def anagrammer(word, idx):
+    key = idx_key(word)
+    return ifilter(lambda a: a.lower() != word.lower(), idx[key])
+
+
+@cli.command()
+@click.option('idxfile', '-i', '--index', type=click.File(),
+              help='input index file')
+@click.option('-w', '--wordsfile', type=click.File(),
+              help='input words file')
 @click.argument('word')
-def main(dictionary, word):
-    if not os.path.exists(dictionary):
-        click.echo('no such file "{}"'.format(dictionary), err=True)
-        sys.exit(1)
-    for anagram in solve_with_file(word, dictionary):
+def solve(idxfile, wordsfile, word):
+    """Solve anagrams for given word."""
+    if idxfile and wordsfile:
+        click.echo('"--index" and "--wordsfile" are mutually exclusive',
+                   err=True)
+        sys.exit(2)
+    if not idxfile and not wordsfile:
+        try:
+            wordsfile = open(default_words_file)
+        except IOError:
+            click.echo('cannot find default file "{}"'
+                       .format(default_words_file), err=True)
+            sys.exit(1)
+    if idxfile:
+        idx = json.load(idxfile)
+    else:
+        idx = mk_idx(wordsfile)
+    for anagram in anagrammer(word, idx):
         click.echo(anagram)
 
+
 if __name__ == '__main__':
-    main()
+    cli()
